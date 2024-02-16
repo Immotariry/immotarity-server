0582f9118136364cae27789fa34ceeafb5d3ebeb
신희성 <vvwv3092@gmail.com>
2024-02-16 23:53:11 +0900

    init

---
+++ src/main/java/com/github/immotarity/ImmotarityApplication.java
@@ -0,0 +1,13 @@
+package com.github.immotarity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ImmotarityApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ImmotarityApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/github/immotarity/ImmotarityApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.immotarity;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ImmotarityApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
